25187065adb4360429b2436c4417aa55f19c122a
sMoscicki <moscicki.szymonm@gmail.com>
2023-04-21 18:00:32 +0200

    Initial Commit

---
+++ JSON Parsing/JSON_ParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSON_ParsingApp.swift
+//  JSON Parsing
+//
+//  Created by s.Moscicki on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct JSON_ParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
