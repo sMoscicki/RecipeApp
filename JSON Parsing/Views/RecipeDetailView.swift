0a5932f8b37145bb8bdde83c2af60600198055d8
sMoscicki <moscicki.szymonm@gmail.com>
2023-04-23 13:24:51 +0200

    For modification

---
+++ JSON Parsing/Views/RecipeDetailView.swift
@@ -0,0 +1,76 @@
+import SwiftUI
+
+struct RecipeDetailView: View {
+    
+    var recipe:Recipe
+    
+    var body: some View {
+        ScrollView{
+            
+            VStack(alignment: .leading){
+                
+                //MARK: Recipe Image
+                    Image(recipe.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 375, height: 150)
+                        .padding([.leading, .trailing], 10)
+                
+                //MARK: Ingredients
+                VStack(alignment: .leading){
+                    Text("Ingredients")
+                        .font(.headline)
+                        .padding([.bottom, .top], 5)
+                        .frame(width: 375)
+                        .background(Color.gray.opacity(0.40))
+                        .cornerRadius(25)
+                    
+                    ForEach(recipe.ingredients, id: \.self){ item in
+                        
+                        Text("•" + item)
+                            .padding(.bottom, 1)
+                    }
+                }
+                .padding(.horizontal)
+                
+                //MARK: Divider
+                Divider()
+                
+                
+                //MARK: Directions
+                VStack(alignment: .leading){
+                    Text("Direction")
+                        .font(.headline)
+                        .padding([.bottom, .top], 5)
+                        .frame(width: 375)
+                        .background(Color.gray.opacity(0.40))
+                        .cornerRadius(25)
+                    
+                    
+                    ForEach(0..<recipe.directions.count, id: \.self){ item in
+                        Text(String(item+1) + ". " + recipe.directions[item])
+                            .padding(.bottom, 5)
+                        
+                        
+                    }
+                    
+                }
+                .padding(.horizontal)
+                
+            }
+            .padding([.leading, .trailing], 10)
+        }
+        .navigationBarTitle(recipe.name)
+    }
+}
+
+struct RecipeDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        //Create a dummy recipe and pass it into the detail view so that we can see a preview
+        
+        let model = RecipeModel()
+        
+        RecipeDetailView(recipe: model.recipes[0])
+    }
+}
--- JSON Parsing/Views/RecipeListView.swift
+++ JSON Parsing/Views/RecipeListView.swift
@@ -6,17 +6,29 @@ struct RecipeListView: View {
     
     var body: some View {
         
-        List(model.recipes){ r in
+        NavigationView{
+            List(model.recipes){ r in
+                
+                NavigationLink(
+                destination: RecipeDetailView(recipe: r),
+                label: {
+                    
+                    //MARK: Row item
+                    HStack(alignment: .center, spacing: 20){
+                        Image(r.image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .clipped()
+                            .cornerRadius(25)
+                        Text(r.name)
+                    }
+                })
+
+                
             
-            HStack(alignment: .center, spacing: 20){
-                Image(r.image)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 50, height: 50, alignment: .center)
-                    .clipped()
-                    .cornerRadius(25)
-                Text(r.name)
             }
+            .navigationBarTitle("All Recipes")
         }
     }
 }
